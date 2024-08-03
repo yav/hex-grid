92dc5a0a4fd8b391840ce1f0fb9b5e2c31f9dc28
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2024-08-03 08:45:26 -0700

    rearrange stuff

--- src/coord.ts
+++ src/coord.ts
@@ -1,7 +1,6 @@
 
 const neighbourTable = [ [1, 0], [0, 1], [-1, 1], [-1, 0], [0,-1], [1,-1] ]
 
-
 const vertexUp: { [name: string]: Dir } = 
   { "E":  0, "SE": 1, "SW": 2, "W": 3, "NW": 4, "NE": 5 }
 
@@ -38,17 +37,6 @@ export function edge_of_face(f: FLoc, d: Dir): ELoc {
   return [ advance(f,d), d - 3 ]
 }
 
-// If edge is facing clockwise, then left hex is first
-export function faces_of_edge([f,d]: ELoc): [FLoc, FLoc] {
-  return [advance(f,d), f]
-}
-
-// Order is if edge is facing clockwise
-export function vertices_of_edge([f,d]: ELoc): [VLoc,VLoc] {
-  return [ vertex_of_face(f,d), vertex_of_face(f,d+1) ]
-}
-
-
 // Vertex on a face in the given direction
 export function vertex_of_face(f: FLoc, d: Dir): VLoc {
   if (d < 2) return [ f, d ]
@@ -62,10 +50,28 @@ export function vertex_of_edge([f,d]: ELoc, n: number): VLoc {
   return vertex_of_face(f,d+n)
 }
 
-// Faces meeting at a vertex
-export function faces_of_vertex([f,d]: VLoc): [FLoc,FLoc,FLoc] {
-  const f1 = advance(f,d)
-  return [ f, f1, advance(f1,turn_counter_clockwise(d))  ]
+
+export function edges_of_face(f: FLoc): ELoc[] {
+  const res = []
+  for (let d = 0; d < 6; ++d)
+     res.push(edge_of_face(f,d))
+  return res
+}
+
+export function vertices_of_face(f: FLoc): VLoc[] {
+  const res = []
+  for (let d = 0; d < 6; ++d)
+     res.push(vertex_of_face(f,d))
+  return res
+}
+
+
+export function faces_of_edge([f,d]: ELoc): [FLoc, FLoc] {
+  return [ advance(f,d), f ]
+}
+
+export function vertices_of_edge([f,d]: ELoc): [VLoc,VLoc] {
+  return [ vertex_of_face(f,d), vertex_of_face(f,d+1) ]
 }
 
 export function edges_of_vertex([f,d]: VLoc): [ELoc,ELoc,ELoc] {
@@ -73,4 +79,10 @@ export function edges_of_vertex([f,d]: VLoc): [ELoc,ELoc,ELoc] {
   const f1 = advance(f,d1)
   if (d == 0) return [ [f1,1], [f1,2], [f,0] ]
   return [ [f,0], [f,1], [f1,2] ]
+}
+
+// Faces meeting at a vertex
+export function faces_of_vertex([f,d]: VLoc): [FLoc,FLoc,FLoc] {
+  const f1 = advance(f,d)
+  return [ f, f1, advance(f1,turn_counter_clockwise(d))  ]
 }
